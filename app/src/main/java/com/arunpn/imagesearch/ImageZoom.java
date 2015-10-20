b9c5700e4b586268ee2b2360673b750ef668fcde
arunpn <oracle.arunp@gmail.com>
2015-10-19 20:06:33 -0700

    Basic features working

---
+++ app/src/main/java/com/arunpn/imagesearch/ImageZoom.java
@@ -0,0 +1,75 @@
+package com.arunpn.imagesearch;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+
+public class ImageZoom extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.imageDetailView)
+    ImageView imageDetailView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_zoom);
+        ButterKnife.bind(this);
+        setupToolBar();
+
+        String url = getIntent().getStringExtra("url");
+
+        Picasso.with(this)
+                .load(url)
+//                .resize(500, 500)
+//                .centerInside()
+                .into(imageDetailView);
+    }
+
+    private void setupToolBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Image Zoom");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.title_bar_text_color));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_image_zoom, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/arunpn/imagesearch/MainActivity.java
+++ app/src/main/java/com/arunpn/imagesearch/MainActivity.java
@@ -2,15 +2,24 @@ package com.arunpn.imagesearch;
 
 import android.app.SearchManager;
 import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.Spinner;
 
 import com.arunpn.imagesearch.adapter.SearchAdapter;
 import com.arunpn.imagesearch.model.ImageDetails;
@@ -25,7 +34,6 @@ import butterknife.ButterKnife;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
-import retrofit.http.Query;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -36,7 +44,19 @@ public class MainActivity extends AppCompatActivity {
     SearchAdapter adapter;
     @Bind(R.id.toolbar)
     Toolbar toolbar;
-    public static  String searchQuery = "";
+    public static String searchQuery = "";
+
+    String colorChoice = "";
+    String imageSizeChoice = "";
+    String siteFilter = "";
+    ArrayAdapter<CharSequence> adColorization;
+    ArrayAdapter<CharSequence> adSize;
+    public static final String API_VERSION = "1.0";
+    public static final int RESULT_SIZE = 8;
+    public static  String IMAGE_SIZE = "";
+    public static  String SEARCH_SITE = "";
+    public static  String IMAGE_COLOR = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +64,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         setupToolBar();
+        storeSharedPrefs("","","");
         adapter = new SearchAdapter(this, new ArrayList<ImageDetails>());
         apiService = new RestClient().getApiService();
-        gridView.setOnScrollListener( new EndlessScrollListener() {
+        gridView.setOnScrollListener(new EndlessScrollListener() {
             @Override
             public boolean onLoadMore(int page, int totalItemsCount) {
                 getMorePhotos(page);
@@ -54,8 +75,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                ImageDetails imageDetails = (ImageDetails) adapterView.getItemAtPosition(i);
+                Intent intent = new Intent(MainActivity.this,ImageZoom.class);
+                intent.putExtra("url",imageDetails.getUrl());
+                startActivity(intent);
+            }
+        });
 
-        getPhotos("",0);
+
+        getPhotos(0);
     }
 
     private void setupToolBar() {
@@ -64,28 +95,23 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitleTextColor(getResources().getColor(R.color.title_bar_text_color));
     }
 
-    private void getPhotos(String query,int page) {
-        
+    private void getPhotos(int page) {
 
-//        String searchQuery = "sachin tendulkar";
-//        String searchQuery = query;
-        String apiVersion = "1.0";
-        int resultSize = 8;
-        int searchPage = page*resultSize;
-        String imageSize="xxlarge";
-        String searchSite="espncricinfo.com";
-        String imageColor="blue";
+        int searchPage = page * RESULT_SIZE;
+        IMAGE_SIZE = returnSharedPref("imageSizeChoice");
+        SEARCH_SITE = returnSharedPref("siteFilter");
+        IMAGE_COLOR = returnSharedPref("colorChoice");
 
-        apiService.getImages(searchQuery,apiVersion,resultSize,searchPage,imageSize,searchSite,imageColor, new Callback<ResponseData>() {
+
+        apiService.getImages(searchQuery, API_VERSION, RESULT_SIZE, searchPage, IMAGE_SIZE, SEARCH_SITE, IMAGE_COLOR, new Callback<ResponseData>() {
             @Override
             public void success(ResponseData responseData, Response response) {
 
-               if (responseData.getCursor().getEstimatedResultCount() > 0) {
-                   gridView.setAdapter(adapter);
-                   adapter.clear();
-                   adapter.addAll(responseData.getResults());
-
-               }
+                if (responseData.getCursor().getEstimatedResultCount() > 0) {
+                    gridView.setAdapter(adapter);
+                    adapter.clear();
+                    adapter.addAll(responseData.getResults());
+                }
             }
 
             @Override
@@ -98,20 +124,15 @@ public class MainActivity extends AppCompatActivity {
 
     private void getMorePhotos(int page) {
 
-//        String searchQuery = "sachin tendulkar";
-//        String searchQuery = query;
-        String apiVersion = "1.0";
-        int resultSize = 8;
-        int searchPage = page*resultSize;
-        String imageSize="xxlarge";
-        String searchSite="espncricinfo.com";
-        String imageColor="blue";
+        int searchPage = page * RESULT_SIZE;
+        IMAGE_SIZE = returnSharedPref("imageSizeChoice");
+        SEARCH_SITE = returnSharedPref("siteFilter");
+        IMAGE_COLOR = returnSharedPref("colorChoice");
 
-        apiService.getImages(searchQuery,apiVersion,resultSize,searchPage,imageSize,searchSite,imageColor, new Callback<ResponseData>() {
+        apiService.getImages(searchQuery, API_VERSION, RESULT_SIZE, searchPage, IMAGE_SIZE, SEARCH_SITE, IMAGE_COLOR, new Callback<ResponseData>() {
             @Override
             public void success(ResponseData responseData, Response response) {
-
-                if (responseData!= null && responseData.getCursor().getEstimatedResultCount() > 0) {
+                if (responseData != null && responseData.getCursor().getEstimatedResultCount() > 0) {
                     adapter.addAll(responseData.getResults());
                 }
             }
@@ -143,11 +164,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         final SearchView finalSearchView = searchView;
-        searchView.setOnQueryTextListener( new SearchView.OnQueryTextListener() {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                searchQuery= query;
-                getPhotos(query,0);
+                searchQuery = query;
+                getPhotos(0);
                 finalSearchView.setQuery("", false);
                 finalSearchView.setIconified(true);
                 return true;
@@ -164,16 +185,105 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            showSettingsDialog();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showSettingsDialog() {
+
+
+        LayoutInflater inflater = getLayoutInflater();
+        View convertView = inflater.inflate(R.layout.settings_layout, null);
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("Advanced Filters").setView(convertView);
+
+        adSize = ArrayAdapter.createFromResource(this,
+                R.array.size_choice, android.R.layout.simple_spinner_item);
+        adSize.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        adColorization = ArrayAdapter.createFromResource(this,
+                R.array.color_choice, android.R.layout.simple_spinner_item);
+        adColorization.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner spSize = (Spinner) convertView.findViewById(R.id.spinnerImageSize);
+        spSize.setAdapter(adSize);
+        Spinner spColorization = (Spinner) convertView.findViewById(R.id.spinnerColorFilter);
+        spColorization.setAdapter(adColorization);
+        final EditText siteFilter = (EditText) convertView.findViewById(R.id.siteEditText);
+        setDefaultSpinnerValues(spSize, spColorization, siteFilter);
+
+
+
+        spSize.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                imageSizeChoice = (String) adapterView.getItemAtPosition(i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+        spColorization.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                colorChoice = (String) adapterView.getItemAtPosition(i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+
+        alertDialog.setPositiveButton("SAVE", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                storeSharedPrefs(colorChoice, imageSizeChoice, siteFilter.getText().toString());
+            }
+        }).setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        }).create();
+        alertDialog.show();
+
+    }
+
+    public void setDefaultSpinnerValues(Spinner spSize, Spinner spColorization, EditText siteFilter) {
+        String value = returnSharedPref("colorChoice");
+        if (value != "") {
+            spColorization.setSelection(adColorization.getPosition(value));
+        }
+        value = returnSharedPref("imageSizeChoice");
+        if (value != "") {
+            spSize.setSelection(adSize.getPosition(value));
+        }
+        value = returnSharedPref("siteFilter");
+        siteFilter.setText(value);
+    }
+
+
+    public void storeSharedPrefs(String colorChoice, String imageSizeChoice, String siteFilter) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("colorChoice", colorChoice);
+        editor.putString("imageSizeChoice", imageSizeChoice);
+        editor.putString("siteFilter", siteFilter);
+        editor.apply();
+    }
+
+    public String returnSharedPref(String choice) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        return preferences.getString(choice, "");
     }
 }
